e3140fc419d33fd27807452686adc135b7cec67f
bogdan <scibs@yandex.ru>
2024-04-04 12:08:05 +0300

    обработка неправильно введенной команды

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -14,5 +14,5 @@ moders = json.loads(env('MODER_ID'))
 @router.message()
 @main_chat
 async def any_message(message: Message, *args, **kwargs):
-    # await message.answer(text='Команда введена некоректно. Для перехода в нужный раздел нажмите кнопку', reply_markup=start_kb(user_id=message.from_user.id))
-    await message.answer(text=f'id чата:\n\n{message.chat.id}')
+    await message.answer(text='Команда введена некоректно. Для перехода в нужный раздел нажмите кнопку', reply_markup=start_kb(user_id=message.from_user.id))
+    # await message.answer(text=f'id чата:\n\n{message.chat.id}')
