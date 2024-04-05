071a3991ebf23525413bf679ba8b6e9b89ea7dce
bogdan <scibs@yandex.ru>
2024-04-05 10:03:42 +0300

    отлавливание ошибки в команде start

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -42,7 +42,7 @@ async def process_start_command(message: Message, *args, **kwargs):
         await message.answer(lexicon_ru['home'], reply_markup=start_kb(user_id))
     except Exception as e:
         session.rollback()
-        await message.answer(text='произошла ошибка в функции process_start_command')
+        await message.answer(text=f'произошла ошибка в функции process_start_command {e.__name__} {e}')
 
     finally:
         session.close()
